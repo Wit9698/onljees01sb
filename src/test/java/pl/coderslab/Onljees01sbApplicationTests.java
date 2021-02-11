694ce2011b76e7754527ded5230daf2c5991e3be
arek-jozwiak-coderslab <Gonsie123>
2021-02-11 16:28:24 +0100

    first commit

---
+++ src/test/java/pl/coderslab/Onljees01sbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onljees01sbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
